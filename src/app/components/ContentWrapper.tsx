86b81eca17b1934f0a10a6aa99ca9d399ed35443
kodomo-kazuko <kodomokazuko@gmail.com>
2024-06-07 10:54:55 +0800

    finished manual

--- src/app/components/ContentWrapper.tsx
+++ src/app/components/ContentWrapper.tsx
@@ -1,28 +1,61 @@
 import React, { ReactNode } from "react";
 import ColorWrapper from "@/app/components/ColorWrapper";
+import Card from "@/app/components/Card";
+import { IPhone } from "@/app/components/iphone";
+import SideButton from "@/app/components/SideButton";
+import Image, { StaticImageData } from "next/image";
+import { useScreenSize } from "../components/getScreenSize";
+
+export type CardData = {
+  title: string;
+  description: string;
+  image: StaticImageData;
+};
 
 interface ContentWrapperProps {
-  children: ReactNode;
+  title: string;
+  cards: CardData[];
 }
 
 const ContentWrapper: React.FC<ContentWrapperProps> = ({
-  children,
+  title,
+  cards,
 }: ContentWrapperProps) => {
   const isMobile =
     typeof window !== "undefined"
       ? window.innerWidth < window.innerHeight
       : false;
 
+  const screenSize = useScreenSize();
+
   return (
-    <div
-      className={`shadow-2xl rounded-3xl ${isMobile ? "flex-col" : "flex-row"}`}
-    >
-      <ColorWrapper color="transparent">
-        <div>
-          <div className="flex justify-around items-center">{children}</div>
-        </div>
-      </ColorWrapper>
-    </div>
+    <>
+      <div className=" text-black text-3xl pl-10">{title}</div>
+      <div
+        className={`shadow-2xl rounded-3xl ${
+          isMobile ? "flex-col" : "flex-row"
+        }`}
+      >
+        <ColorWrapper color="transparent">
+          <div>
+            <div className="flex justify-around items-center">
+              {cards.map((card, index) => (
+                <>
+                  <Card title={card.title} description={card.description}>
+                    <div className="flex w-11/12 md:w-7/10 mx-auto justify-center">
+                      <IPhone boxShadow={undefined} width={screenSize}>
+                        <Image alt="" src={card.image} fill priority />
+                      </IPhone>
+                    </div>
+                  </Card>
+                  {index < cards.length - 1 && <SideButton />}
+                </>
+              ))}
+            </div>
+          </div>
+        </ColorWrapper>
+      </div>
+    </>
   );
 };
 
---
+++ src/app/page/manualSections/Direction2.tsx
@@ -0,0 +1,28 @@
+import image_1 from "../../../../public/images/manual/6_direction_4.png";
+import image_2 from "../../../../public/images/manual/6_direction_5.png";
+import image_3 from "../../../../public/images/manual/6_direction_6.png";
+import ContentWrapper, { CardData } from "@/app/components/ContentWrapper";
+
+export default function Direction2() {
+  const cards: CardData[] = [
+    {
+      title: "Зогсоолын дэлгэрэнгүй",
+      description:
+        "Сонгосон автобусны зогсоол дээр ирэх чиглэлийн автобуснууд харагдана.",
+      image: image_1,
+    },
+    {
+      title: "Чиглэлийн дэлгэрэнгүй",
+      description:
+        "Сонгосон чиглэлийн автобуснууд хаана явж байгаа нь жагсаалт хэлбэрээр харагдана.",
+      image: image_2,
+    },
+    {
+      title: "Чиглэлийн маршрут",
+      description: "Мөн газрын зураг дээрээс автобусны байршилыг харж болно.",
+      image: image_3,
+    },
+  ];
+
+  return <ContentWrapper title="" cards={cards} />;
+}
