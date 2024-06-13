7e207c7d3c037b9f8945fd58599a9baedce1fdc0
kodomo-kazuko <kodomokazuko@gmail.com>
2024-06-13 16:49:40 +0800

    reverted changes

--- src/app/wrappers/ContentWrapper.tsx
+++ src/app/wrappers/ContentWrapper.tsx
@@ -5,6 +5,7 @@ import { IPhone } from "@/app/components/iphone";
 import SideButton from "@/app/components/SideButton";
 import Image, { StaticImageData } from "next/image";
 import { useScreenSize } from "../components/getScreenSize";
+import SwipeWrapper from "./SwipeWrapper";
 
 export type CardData = {
   title: string;
@@ -29,35 +30,35 @@ const ContentWrapper: React.FC<ContentWrapperProps> = ({
   const { screenSize } = useScreenSize({});
 
   return (
-    <div className={` flex ${isMobile ? "flex-col" : "flex-row"}`}>
-      <ColorWrapper color="transparent">
-        <ColorWrapper color={"secondary"}>
-          <div className="text-center text-3xl pl-8 md:text-4xl">{title}</div>
-        </ColorWrapper>
-        <div>
-          <div
-            className={`flex ${
-              isMobile ? "flex-col space-y-8" : "flex-row space-x-8"
-            } justify-around items-center overflow-auto`}
-          >
-            {cards.map((card, index) => (
-              <>
-                <Card title={card.title} description={card.description}>
-                  <div className="flex w-11/12 md:w-7/10 mx-auto justify-center">
-                    <IPhone boxShadow={undefined} width={screenSize}>
-                      <Image alt="" src={card.image} priority fill />
-                    </IPhone>
-                  </div>
-                </Card>
-                {index < cards.length - 1 && (
-                  <SideButton direction={isMobile ? "down" : "right"} />
-                )}
-              </>
-            ))}
+    <SwipeWrapper animation={"fade-right"}>
+      <div className={` flex ${isMobile ? "flex-col" : "flex-row"}`}>
+        <ColorWrapper color="transparent">
+          <div className="text-black text-3xl pl-8 md:text-4xl">{title}</div>
+          <div>
+            <div
+              className={`flex ${
+                isMobile ? "flex-col space-y-8" : "flex-row space-x-8"
+              } justify-around items-center overflow-auto`}
+            >
+              {cards.map((card, index) => (
+                <>
+                  <Card title={card.title} description={card.description}>
+                    <div className="flex w-11/12 md:w-7/10 mx-auto justify-center">
+                      <IPhone boxShadow={undefined} width={screenSize}>
+                        <Image alt="" src={card.image} priority fill />
+                      </IPhone>
+                    </div>
+                  </Card>
+                  {index < cards.length - 1 && (
+                    <SideButton direction={isMobile ? "down" : "right"} />
+                  )}
+                </>
+              ))}
+            </div>
           </div>
-        </div>
-      </ColorWrapper>
-    </div>
+        </ColorWrapper>
+      </div>
+    </SwipeWrapper>
   );
 };
 
