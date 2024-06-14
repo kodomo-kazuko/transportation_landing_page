416f49d0ff997f225ae037ec4ca316a4d4460fa6
kodomo-kazuko <kodomokazuko@gmail.com>
2024-06-14 12:49:36 +0800

    made text bigger

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -11,8 +11,12 @@ export default function Card({ title, description, children }: CardProps) {
   return (
     <div className="flex-grow rounded-lg p-4 space-y-4 md:p-8 md:space-y-8 text-left">
       <div className="h-20 md:h-24">
-        <h1 className="text-xl font-bold md:text-2xl">{title}</h1>
-        <p className="text-sm md:text-base">{description}</p>
+        <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold">
+          {title}
+        </h1>
+        <p className="text-sm md:text-base lg:text-lg xl:text-xl">
+          {description}
+        </p>
       </div>
       <div className="items-end">{children}</div>
     </div>
