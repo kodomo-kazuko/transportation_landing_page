7a4fc66db18f8f11dcbe93b2122fd0657013abf7
kodomo-kazuko <kodomokazuko@gmail.com>
2024-06-17 17:38:36 +0800

    further fixes to manual responsive

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 export default function Card({ title, description, children }: CardProps) {
   return (
-    <div className="rounded-lg p-4 space-y-10 sm:space-y-4  md:space-y-8 md:w-2/4 text-left">
+    <div className="rounded-lg px-10 py-5 sm:p-4 space-y-10 sm:space-y-4  md:space-y-8 md:w-2/4 text-left">
       <div className="h-20 md:h-24">
         <h1 className="text-xl font-bold lg:text-2xl">{title}</h1>
         <p className="text-md md:text-base">{description}</p>
--- src/app/wrappers/ContentWrapper.tsx
+++ src/app/wrappers/ContentWrapper.tsx
@@ -36,8 +36,8 @@ const ContentWrapper: React.FC<ContentWrapperProps> = ({
   return (
     <div className={` flex  sm:flex-col  md:flex-row w-full"}`}>
       <ColorWrapper color="transparent" className=" w-full">
-        <div className=" text-ub-primary text-3xl text-center md:px-4 md:text-4xl ">
-          <ColorWrapper color="primary" className={`rounded-2xl py-5 ${className}`}>
+        <div className=" text-ub-primary text-3xl text-center px-5 sm:p-4 md:text-4xl ">
+          <ColorWrapper color="primary" className={`rounded-2xl py-5 ${className} `}>
             {title}</ColorWrapper>
         </div>
         <div>
