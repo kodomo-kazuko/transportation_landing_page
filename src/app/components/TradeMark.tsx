00eb262c2e7f4fed46b2db4f801ca18b46b63dc5
kodomo-kazuko <kodomokazuko@gmail.com>
2024-06-17 14:20:20 +0800

    trademark svg size

--- src/app/components/TradeMark.tsx
+++ src/app/components/TradeMark.tsx
@@ -2,9 +2,9 @@ import theirIcon from '../../../public/images/Icon/Niitiin-teever-logo 1 (1).svg
 import Image from "next/image";
 
 export default function TradeMark() {
-    const size: number = 50
+    const size: number = 30
     return (
-        <div className="flex justify-center items-center pb-2">
+        <div className="flex justify-center items-center pb-5">
             <Image src={theirIcon} alt="Icon" className="mr-2" height={size} width={size} />
             <p className="text-gray-500 text-sm">Нийтийн Тээврийн Газар ©2024</p>
         </div>
